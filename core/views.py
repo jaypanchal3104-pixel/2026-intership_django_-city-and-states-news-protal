484aef6ef6d786ac451567c6bd7f02687d5c956b
jaypanchal3104-pixel <jaypanchal3104@gmail.com>
2026-02-26 10:45:34 +0530

    signup login

--- core/views.py
+++ core/views.py
@@ -24,7 +24,7 @@ def userLoginView(request):
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
 
-            user = authenticate(request, email=email, password=password)
+            user = authenticate(request, username=email, password=password)
 
             if user is not None:
                 login(request, user)
